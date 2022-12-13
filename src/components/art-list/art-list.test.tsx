e1421bb5184b0ac29417794b7c03a85d2929e7c1
MENAxDesign <menelik.simpson@xdesign.com>
2022-12-13 16:56:35 +0000

    [TESTID-123] Add unit test to art list
    This change unit tests that the artist list component returns a list of two art items from the Art Institute of Chicago's API. Flyby change: Fixes json formatting in mock service worker handler file

--- src/components/art-list/art-list.test.tsx
+++ src/components/art-list/art-list.test.tsx
@@ -1,6 +1,10 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ArtListComponent } from "./art-list";
 
-describe("Art Item", () => {
-
+describe("Art List", () => {
+  test.only("displays a list of art items from the server", async () => {
+    render(<ArtListComponent/>)
+    const artItems = await screen.findAllByAltText(/artwork/i)
+    expect(artItems).toHaveLength(2);
+  });
 });
